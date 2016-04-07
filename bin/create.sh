161ad574d9e875be1b94f5af1d72f0220bb5d450
craigmiller160 <craigmiller160@gmail.com>
2016-04-07 15:37:23 -0400

    Updated create.sh to work with myvcs2, and fixed a few remaining myvcs2 configuration bugs

--- bin/create.sh
+++ bin/create.sh
@@ -12,40 +12,31 @@
 #			[branch name] : (Required) The name of the branch to create, both in Git
 #									and in SVN.
 
-# Get shell script's directory and move shell there to execute config script
-# SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
-source ./global.sh
-
 # The name of the branch to create.
 NAME=""
 
-
-###############
-# TODO need a way to cancel the process if any conflicts occur
-# TODO need a way to suppress SVN/Git output withouth loosing the ability to handle conflicts
-# TODO make sure everything gets tested with potential merge conflicts lined up
-# TODO need to test that the branch doesn't exist already, in Git, on filesystem
-# TODO need to create custom workspace.xml & vcs.xml after new dir exists
-###############
-
-# echo "THIS COMMAND IS CURRENTLY IN THE PROCES OF BEING MODIFIED AND DOESN'T WORK"
-# exit 1
-
-# Test that there is at least one parameter for this command.
-# Param $1 will always be the command name, so there need to be
+# Test that there is at least two parameters for this command.
+# Param $2 will always be the command name, so there need to be
 #   at least two total params for this script to be able to run
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
 	printf "${RED}${BOLD}$TAG Error! Invalid number of parameters supplied to create command.${NORM}${NC}\n"
 	exit 1
 fi
 
+
+
+# Set the MYVCS_PATH variable and source bin files
+MYVCS_PATH="$1"
+source "$MYVCS_PATH/myvcs-config.properties"
+source "$MYVCS_PATH/bin/global.sh"
+
+echo "$DEV_MAIN_PATH"
+
 # If there is only one parameter (two total, including the command),
 #   then that parameter MUST be the name of the branch. If it is another
 #   parameter value, then an error will be thrown.
-if [ $# -eq 1 ]; then
-	case "$1" in
+if [ $# -eq 2 ]; then
+	case "$2" in
 		# If it has a parameter prefix (-) or (--), then it is an invalid argument
 		-* | --*)
 			printf "${RED}${BOLD}$TAG Error! No valid branch name supplied. Branch names cannot start with parameter prefixes (-) and (--).${NORM}${NC}\n"
@@ -53,7 +44,7 @@ if [ $# -eq 1 ]; then
 		;;
 		# Otherwise, set it as the branch name variable
 		*)
-			NAME="$1"
+			NAME="$2"
 		;;
 	esac
 fi
@@ -75,10 +66,6 @@ svn_switch_func "trunk"
 if [ $? -ne 0 ]; then
 	return 1
 fi
-
-#####################
-### TODO consider adding an update-from-trunk thing here
-#####################
 
 # Test if there are any files that SVN sees having changes, and revert them
 #   Git Trunk should always be a pure version of SVN's Trunk, with no files differing
@@ -128,8 +115,8 @@ git branch -u origin/"$NAME" 1>/dev/null 2>/dev/null
 echo "$TAG Configuring IntelliJ preferences"
 git update-index --assume-unchanged "$DEV_ROOT_PATH/$NAME/.idea/vcs.xml"
 rm -f "$DEV_ROOT_PATH/$NAME/.idea/vcs.xml"
-cp "$HOME/myvcs/bin/vcs.xml" "$DEV_ROOT_PATH/$NAME/.idea" #### TODO make this more portable, less specific path-wise
-cp "$HOME/myvcs/bin/workspace.xml" "$DEV_ROOT_PATH/$NAME/.idea" #### TODO make this more portable, less specific path-wise
+cp "$MYVCS_PATH/bin/vcs.xml" "$DEV_ROOT_PATH/$NAME/.idea"
+cp "$MYVCS_PATH/bin/workspace.xml" "$DEV_ROOT_PATH/$NAME/.idea"
 
 echo "$TAG New local Git branch & directory set up for [$NAME]."
 exit 0
